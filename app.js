fa7710733a5d68c96f458a80cc0a6e61cefe8e1c
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-02-20 22:06:35 +1100

    set up express server and ajax in html

---
+++ app.js
@@ -0,0 +1,20 @@
+// const express = require('express')
+// const app = express()
+
+// // app.get('/', (req, res) => res.send('/index.html'))
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use('/public',express.static(path.join(__dirname, 'public')));
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
